6f1a3ba4fe5bd502c17d61c1d394b76cbfa11d92
S@2g <S@2g>
2022-09-12 23:05:54 +0800

    提交项目

--- src/main/java/com/nu1r/jndi/gadgets/utils/jre/SerializedElement.java
+++
@@ -1,8 +0,0 @@
-package com.nu1r.jndi.gadgets.utils.jre;
-
-import java.io.DataOutputStream;
-
-public interface SerializedElement {
-
-	void write(DataOutputStream paramDataOutputStream, HandleContainer paramHandleContainer) throws Exception;
-}
