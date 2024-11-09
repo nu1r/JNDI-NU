dff3df303ff67e2bd0e3a4c5c0e0dab9af04062e
qi4l <nuxm771@163.com>
2024-11-09 14:46:52 +0800

    fix: Onlyref 2

--- src/main/java/com/qi4l/JYso/controllers/OnlyrefController.java
+++ src/main/java/com/qi4l/JYso/controllers/OnlyrefController.java
@@ -23,7 +23,7 @@ import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.Enumeration;
 
-@LdapMapping(uri = {"/Onlyref"})
+@LdapMapping(uri = {"/onlyref"})
 public class OnlyrefController implements LdapController {
     private String     payloadType;
     private String[]   params;
